38d65e8a4ebeac691b909460052bbe7b05988bff
souquet ludovic <l.souquet@codeur.online>
2022-05-31 16:57:43 +0200

    commencement header, section 1 2 3

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/section1.css">
+    <link rel="stylesheet" href="./css/section2.css">
+    <link rel="stylesheet" href="./css/section3.css">
+    <link rel="stylesheet" href="./css/font.css">
+    <title>Portfolio Ludovic Souquet</title>
+</head>
+<header>
+    <ul>
+        <li>formation</li>
+        <li>projet</li>
+        <li>langage</li>
+        <li>contact</li>
+    </ul>
+</header>
+<body>
+    <section class="section_1">
+        <img class="logo" src="./img/logo_kitsune.png" alt="logo_portfolio">
+        <h1>souquet ludovic</h1>
+        <p class="devweb">Developpeur Web</p>
+    </section>
+    <section class="section2">
+        <div class="moi">
+            <h2>A propos de moi</h2>
+            <p class="description_moi">hello !! je m'appelle Ludovic Souquet, actuellement en formation </br>
+            developpeur web, j'ai déja fais plusieurs catégories de métier qui mon permis </br>
+            d'aquérir de l'expérience et des compétence tel que l'organisation </br>
+            ou l'autonomie, ayant toujours eu une attirance pour l'informatique et le codage, </br>
+            je me suis dirigée vers cette formation qui mélange apprentissage de connaissance et travaille d'équipe. </br>
+            la curiosité, l'envie d'avancer et la motivation fond aussi partie de mon vocabulaire. </br>
+            n'hésitez pas a me contacter
+            </p>
+        </div>
+    </section>
+    <section class="section3">
+        <div class="jadoo">
+            <p>Jadoo</p>
+            <img src="./img/jadoo_1.png" alt="image jadoo">
+        </div> 
+        <div class="voyage">
+            <p>voyage</p>
+            <img src="./img/voyage_1.png" alt="image voyage">
+        </div> 
+        <div class="retro">
+            <p>retro-games</p>
+            <img src="./img/retro_1.png" alt="image retro-games">
+        </div>
+    </section>
+    
+</body>
+</html>
